61a70ba28e757a5e9be0a4caf4571c6f94e83f50
MacMaster <andrew.macmaster@fiserv.com>
2023-11-14 10:41:12 -0500

    ex2

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from src.assignments import *
 
 
 def main():
-    ex1()
-
+    # ex1()
+    ex2()
 
 if __name__ == '__main__':
     main()
--- src/assignments.py
+++ src/assignments.py
@@ -13,3 +13,16 @@ def ex1():
 
     sort_people(people_list, 'weight', 'asc')
     print(people_list)
+
+def ex2():
+    people_list = [
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+
+    def filter_males(people):
+        return list(filter(lambda p: p['sex'] == 'male', people))
+    
+    filtered_list = filter_males(people_list)
+    print(filtered_list)
