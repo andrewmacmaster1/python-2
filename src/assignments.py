6cb877b1b9f4a55d406d9ab766f48f535ddf8944
MacMaster <andrew.macmaster@fiserv.com>
2023-11-14 10:38:19 -0500

    ex1

--- src/assignments.py
+++ src/assignments.py
@@ -1,2 +1,15 @@
 # define your methods here.
 # ex1() - ex10()
+
+def ex1():
+    people_list = [
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+
+    def sort_people(people, field, direction):
+        people.sort(reverse=(direction=='desc'),key=lambda p: p[field])
+
+    sort_people(people_list, 'weight', 'asc')
+    print(people_list)
